384044d07a271e619ee55df0653e7502258fd1a8
DavideGila <davide.gila@gmail.com>
2023-12-04 15:22:01 +0100

    made a foreach to start prinCard function

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include __DIR__ . "/Views/header.php";
 include __DIR__ . "/Model/Movie.php";
 ?>
@@ -6,7 +6,11 @@ include __DIR__ . "/Model/Movie.php";
 <section class="container">
     <h2>Movie</h2>
     <div class="row">
-        
+        <?php
+        foreach ($movies as $movie) {
+            $movie->printCard();
+        }
+        ?>
     </div>
 </section>
 
