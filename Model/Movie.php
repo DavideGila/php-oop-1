7e2717c20a642c5f92ad937253d405f2516ab0cb
DavideGila <davide.gila@gmail.com>
2023-12-04 16:24:21 +0100

    created a function that make appear the vote in stars from 1 to 5

--- Model/Movie.php
+++ Model/Movie.php
@@ -27,9 +27,20 @@ class Movie
         $image = $this->poster_path;
         $title = $this->title;
         $content = $this->overview;
-        $custom = $this->vote_average;
+        $custom = $this->getVote();
         $genre = $this->genre;
         include __DIR__ . "/../Views/card.php";
+    }
+
+    public function getVote()
+    {
+        $vote = ceil($this->vote_average / 2);
+        $template = "<p>";
+        for ($n = 1; $n <= 5; $n++) {
+            $template .= $n <= $vote ? '<i class="fa-solid fa-star text-warning"></i>' : '<i class="fa-regular fa-star text-dark"></i>';
+        }
+        $template .= '</p>';
+        return $template;
     }
 }
 
