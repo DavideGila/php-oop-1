f7992b6928efd0ab37b4002a0129451658edc210
DavideGila <davide.gila@gmail.com>
2023-12-04 15:13:12 +0100

    imported and decoded movie json

--- Model/Movie.php
+++ Model/Movie.php
@@ -1,20 +1,27 @@
 <?php
-    class Movie
-    {
-        private int $id;
-        private string $title;
-        private string $overview;
-        private float $vote_average;
-        private string $poster_path;
-        private string $original_language;
+class Movie
+{
+    private int $id;
+    private string $title;
+    private string $overview;
+    private float $vote_average;
+    private string $poster_path;
+    private string $original_language;
 
-        function __construct($id, $title, $overview, $vote, $poster_path, $original_language){
-            $this->id = $id;
-            $this->title = $title;
-            $this->overview = $overview;
-            $this->vote_average = $vote;
-            $this->poster_path = $poster_path;
-            $this->original_language = $original_language;
-        }
+    function __construct($id, $title, $overview, $vote, $poster_path, $original_language)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->overview = $overview;
+        $this->vote_average = $vote;
+        $this->poster_path = $poster_path;
+        $this->original_language = $original_language;
     }
-?>
+}
+
+$movieString = file_get_contents(__DIR__ . '/movie_db.json');
+$movieList = json_decode($movieString, true);
+$movies = [];
+foreach ($movieList as $item) {
+    $movies[] = new Movie($item['id'], $item['title'], $item['overview'], $item['vote_average'], $item['poster_path'], $item['original_language']);
+}
