96e499f1bc135890e58af3c2dc017c4a6b66bb7e
DavideGila <davide.gila@gmail.com>
2023-12-04 14:57:21 +0100

    created movie class

---
+++ Model/Movie.php
@@ -0,0 +1,11 @@
+<?php
+    class Movie
+    {
+        private int $id;
+        private string $title;
+        private string $overview;
+        private int $vote_average;
+        private string $poster_path;
+        private string $original_language;
+    }
+?>
