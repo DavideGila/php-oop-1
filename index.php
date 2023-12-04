ad7b35cbc9ee76fac40c6ca87a4931f58464679f
DavideGila <davide.gila@gmail.com>
2023-12-04 14:50:59 +0100

    included movie, header and footer

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+include __DIR__ . "/Views/header.php";
+include __DIR__ . "/Model/Movie.php";
+?>
+
+<section class="container">
+    <h2>Movie</h2>
+    <div class="row">
+        
+    </div>
+</section>
+
+<?php
+include __DIR__ . "/Views/footer.php";
+?>
