68d3476903ff6a0f17b393aa50fb10f9a73bedbf
DavideGila <davide.gila@gmail.com>
2023-12-04 15:07:08 +0100

    created construct

--- Model/Movie.php
+++ Model/Movie.php
@@ -4,8 +4,17 @@
         private int $id;
         private string $title;
         private string $overview;
-        private int $vote_average;
+        private float $vote_average;
         private string $poster_path;
         private string $original_language;
+
+        function __construct($id, $title, $overview, $vote, $poster_path, $original_language){
+            $this->id = $id;
+            $this->title = $title;
+            $this->overview = $overview;
+            $this->vote_average = $vote;
+            $this->poster_path = $poster_path;
+            $this->original_language = $original_language;
+        }
     }
 ?>
